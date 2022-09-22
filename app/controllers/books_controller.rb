918eadfedac3f650cd6bbf23b65a0ca5bec605f5
WataruNishimura <110705525+wataru1199@users.noreply.github.com>
2022-09-22 12:34:30 +0000

    [Start]4-25

---
+++ app/controllers/books_controller.rb
@@ -0,0 +1,50 @@
+class BooksController < ApplicationController
+  def new
+    @book = Book.new
+  end
+
+  def index
+    @books = Book.all
+
+  end
+  
+  def create
+    @book = Book.new(book_params) 
+    @book.user_id = current_user.id
+    @book.save 
+    redirect_to book_path(@book.id)
+
+  end
+
+  def show
+    @book = Book.find(params[:id])
+  end
+  
+  def edit
+    @book = Book.find(params[:id])
+    @title = @book.title
+    @body = @book.body
+  end
+  
+  def destroy
+    @book = Book.find(params[:id])
+    @book.destroy
+    redirect_to books_path
+  end
+  
+  def update
+      @book = Book.find(params[:id])
+   if @book.update(book_params)
+      redirect_to book_path(@book.id)
+   else
+      render :edit
+   end
+  end
+  
+  private
+
+  def book_params
+    params.require(:book).permit(:title, :body,)
+  end
+
+end
---
+++ config/routes.rb
@@ -0,0 +1,15 @@
+Rails.application.routes.draw do
+
+  devise_for :users
+  root to: 'homes#top'
+  get 'about' => "homes#about" ,as: 'about'
+
+
+  resources :books, only: [:new, :index, :show, :create, :edit, :destroy, :update]
+  resources :users, only: [:show, :edit, :index, :update]
+
+
+
+
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+end
